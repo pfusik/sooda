2023e566303a2e3fd6d88d07c67547d43240d837
Jaroslaw Kowalski <jaak@jkowalski.net>
2004-07-30 20:29:58 +0200

    added missing files

---
+++ src/Tools/SoodaSchemaTool/ConsoleRunner.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Xml;
+
+using Sooda.Schema;
+using Sooda.Sql;
+
+public class ConsoleRunner
+{
+    private static void GenerateDDLForSchema(string schemaFileName)
+    {
+        XmlTextReader xr = new XmlTextReader(schemaFileName);
+        SchemaInfo schemaInfo = SchemaManager.ReadAndValidateSchema(xr);
+
+        SqlDataSource sds = new SqlDataSource("default");
+        sds.SqlBuilder = new SqlServerBuilder();
+        sds.GenerateDdlForSchema(schemaInfo, Console.Out);
+    }
+
+    public static int Main(string[] args)
+    {
+        switch (args[0])
+        {
+            case "genddl":
+                GenerateDDLForSchema(args[1]);
+                break;
+        }
+        return 0;
+    }
+}
