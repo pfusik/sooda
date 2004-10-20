bd15160131bc6b6bfe8dc687169c36bdb2837be5
Jaroslaw Kowalski <jaak@jkowalski.net>
2004-10-20 14:42:02 +0200

    added SoodaFixKeygen utility

---
+++ src/Tools/SoodaFixKeygen/Class1.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using System.Xml;
+using Sooda.Schema;
+using System.IO;
+
+namespace SoodaFixKeygen
+{
+	class Class1
+	{
+        static int Main(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Usage: SoodaFixKeyGen [schemafilename] [output_sql]");
+                return 1;
+            }
+            SchemaInfo schemaInfo;
+
+            XmlTextReader reader = new XmlTextReader(args[0]);
+            schemaInfo = SchemaManager.ReadAndValidateSchema(reader);
+            reader.Close();
+
+            using (StreamWriter output = new StreamWriter(args[1], false, System.Text.Encoding.Default))
+            {
+                output.WriteLine("delete from KeyGen;");
+                bool first = true;
+                output.WriteLine("insert into KeyGen");
+                foreach (ClassInfo classInfo in schemaInfo.Classes)
+                {
+                    FieldInfo fieldInfo = classInfo.GetPrimaryKeyField();
+                    if (fieldInfo.DataType == FieldDataType.Integer)
+                    {
+                        if (!first)
+                            output.WriteLine("union");
+                    
+                        output.WriteLine("select '{0}',coalesce(max({1}),0) + 1 from {2}", 
+                            classInfo.Name, fieldInfo.DBColumnName, classInfo.UnifiedTables[0].DBTableName);
+                        first = false;
+                    }
+                }
+            }
+            return 0;
+        }
+	}
+}
