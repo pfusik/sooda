04be114025e94ee424e9067e0f4e3bd6947d5f1e
Jaroslaw Kowalski <jaak@jkowalski.net>
2006-09-08 13:16:52 +0200

    cleanup

--- src/Sooda.CodeGen/AssemblyBuildInfo.cs
+++
@@ -1,4 +0,0 @@
-// do not modify this file. It will be automatically regenerated
-// based on the version number saved in 'D:\Work\Sooda\src\Sooda.CodeGen\..\..\Sooda.version'
-using System.Reflection;
-[assembly: AssemblyVersion("1.0.0.371")]
