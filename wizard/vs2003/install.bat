2386cca85ca7ae5fd0d3b7bc16318543b1c426e0
Jaroslaw Kowalski <jaak@jkowalski.net>
2006-09-08 13:33:57 +0200

    cleanup

--- wizard/vs2003/install.bat
+++
@@ -1,4 +0,0 @@
-set VC71DIR=C:\Program Files\Microsoft Visual Studio .NET 2003\VC#
-xcopy /y /s VC#Wizards "%VC71DIR%\VC#Wizards"
-xcopy /y /s CSharpProjects "%VC71DIR%\CSharpProjects"
-xcopy /y /s CSharpProjectItems "%VC71DIR%\CSharpProjectItems"
