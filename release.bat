d5ae7fef0f36d98c00fe3d03e338f46714602327
Jaroslaw Kowalski <jaak@jkowalski.net>
2006-09-15 16:12:43 +0200

    release

--- release.bat
+++ release.bat
@@ -1,5 +1,6 @@
-nant source_snapshot
-nant -t:net-1.1 clean release build binary_snapshot
-nant -t:net-1.1 clean debug build binary_snapshot
-nant -t:net-2.0 clean release build binary_snapshot installer
-nant -t:net-2.0 clean debug build binary_snapshot installer
+set ARGS=%*
+nant -t:net-1.1 %ARGS% clean release build binary_snapshot
+nant -t:net-1.1 %ARGS% clean debug build binary_snapshot
+nant -t:net-2.0 %ARGS% clean release build binary_snapshot installer
+nant -t:net-2.0 %ARGS% clean debug build binary_snapshot installer
+nant %ARGS% source_snapshot
