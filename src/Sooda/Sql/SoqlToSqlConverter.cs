b5c0a06d2dd69a95efbcdd046c0b93b844d23195
Maciej Figatowski <mfigatowski@users.sourceforge.net>
2008-09-07 23:49:55 +0200

    FUNCION "UpperLike" setting works also on "=" (for full support for case-sensitive Oracle databases)

--- src/Sooda/Sql/SoqlToSqlConverter.cs
+++ src/Sooda/Sql/SoqlToSqlConverter.cs
@@ -1227,7 +1227,7 @@ namespace Sooda.Sql
 
         public override void Visit(SoqlBooleanRelationalExpression v)
         {
-            bool upper = UpperLike && v.op == SoqlRelationalOperator.Like;
+            bool upper = UpperLike && ((v.op == SoqlRelationalOperator.Like) || (v.op == SoqlRelationalOperator.Equal));
 
             //
             // this is to support type coercions. Whenever we have
