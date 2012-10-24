14f4ccdb3bc156579c890d95739284b8aa156a97
Piotr Fusik <piotr@fusik.info>
2012-10-24 18:20:43 +0200

    FUNCTION LINQ: comparisons of range variable.

--- src/Sooda/Linq/SoodaQueryProvider.cs
+++ src/Sooda/Linq/SoodaQueryProvider.cs
@@ -138,10 +138,18 @@ namespace Sooda.Linq
             }
         }
 
+        static SoqlLiteralExpression Literal(object value)
+        {
+            SoodaObject so = value as SoodaObject;
+            if (so != null)
+                value = so.GetPrimaryKeyValue();
+            return new SoqlLiteralExpression(value);
+        }
+
         static SoqlLiteralExpression FoldConstant(Expression expr, Func<string> error)
         {
             if (IsConstant(expr))
-                return new SoqlLiteralExpression(Expression.Lambda(expr).Compile().DynamicInvoke(null));
+                return Literal(Expression.Lambda(expr).Compile().DynamicInvoke(null));
             throw new NotSupportedException(error());
         }
 
@@ -338,16 +346,7 @@ namespace Sooda.Linq
             if (cwg != null && cwg.IsGenericType && cwg.GetGenericTypeDefinition() == typeof(SoodaObjectCollectionWrapperGeneric<>) && mc.Method.Name == "Contains")
             {
                 SoqlPathExpression haystack = (SoqlPathExpression) TranslateExpression(mc.Object);
-                SoqlExpression needle;
-                if (mc.Arguments[0].NodeType == ExpressionType.Parameter)
-                {
-                    Sooda.Schema.FieldInfo[] pks = _classInfo.GetPrimaryKeyFields();
-                    if (pks.Length != 1)
-                        throw new NotSupportedException(t.FullName + ".Contains(composite_primary_key)");
-                    needle = new SoqlPathExpression(pks[0].Name);
-                }
-                else
-                    needle = TranslateExpression(mc.Arguments[0]);
+                SoqlExpression needle = TranslateExpression(mc.Arguments[0]);
                 return new SoqlContainsExpression(haystack.Left, haystack.PropertyName, needle);
             }
 
@@ -364,7 +363,12 @@ namespace Sooda.Linq
             switch (expr.NodeType)
             {
                 case ExpressionType.Constant:
-                    return new SoqlLiteralExpression(((ConstantExpression) expr).Value);
+                    return Literal(((ConstantExpression) expr).Value);
+                case ExpressionType.Parameter:
+                    Sooda.Schema.FieldInfo[] pks = _classInfo.GetPrimaryKeyFields();
+                    if (pks.Length != 1)
+                        throw new NotSupportedException("Comparison of composite primary key)");
+                    return new SoqlPathExpression(pks[0].Name);
                 case ExpressionType.MemberAccess:
                     return TranslateMember((MemberExpression) expr);
                 case ExpressionType.Add:
