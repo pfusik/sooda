f570ba92d77b7da524e2b98a8d1805d21706d040
Piotr Fusik <piotr@fusik.info>
2014-05-26 21:11:37 +0200

    TEST Transaction: no DB connection if not needed, possible to pass DB connection.

---
+++ tests/TestCases/TransactionTest.cs
@@ -0,0 +1,76 @@
+// 
+// Copyright (c) 2014 Piotr Fusik <piotr@fusik.info>
+// 
+// All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or without 
+// modification, are permitted provided that the following conditions 
+// are met:
+// 
+// * Redistributions of source code must retain the above copyright notice, 
+//   this list of conditions and the following disclaimer. 
+// 
+// * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution. 
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE 
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF 
+// THE POSSIBILITY OF SUCH DAMAGE.
+// 
+
+using System.Data;
+
+using Sooda;
+
+using Sooda.UnitTests.BaseObjects;
+
+using NUnit.Framework;
+
+namespace Sooda.UnitTests.TestCases
+{
+    [TestFixture]
+    public class TransactionTest
+    {
+        [Test]
+        public void LazyDbConnection()
+        {
+            using (new SoodaTransaction())
+            {
+            }
+        }
+
+        [Test]
+        public void PassDbConnection()
+        {
+            using (SoodaDataSource sds = _DatabaseSchema.GetSchema().GetDataSourceInfo("default").CreateDataSource())
+            {
+                sds.Open();
+                // sds.ExecuteNonQuery(sql, params);
+                using (IDataReader r = sds.ExecuteRawQuery("select count(*) from contact"))
+                {
+                    bool b = r.Read();
+                    Assert.IsTrue(b);
+                    int c = r.GetInt32(0);
+                    Assert.AreEqual(7, c);
+                }
+
+                using (SoodaTransaction tran = new SoodaTransaction())
+                {
+                    tran.RegisterDataSource(sds);
+
+                    int c = Contact.GetList(true).Count;
+                    Assert.AreEqual(7, c);
+                }
+            }
+        }
+    }
+}
