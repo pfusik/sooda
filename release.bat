58911830b157bbaf1838192860a20ed576f7de4a
Piotr Fusik <piotr@fusik.info>
2014-10-13 15:08:00 +0200

    CLEANUP Build: remove precommitcheck, release.bat.

--- release.bat
+++
@@ -1,5 +0,0 @@
-set ARGS=%*
-nant %ARGS% doc
-nant -t:net-2.0 %ARGS% clean release build binary_snapshot installer
-nant -t:net-2.0 %ARGS% clean debug build binary_snapshot installer
-nant %ARGS% source_snapshot
